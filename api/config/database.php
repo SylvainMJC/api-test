4e9d19c3879be21744b780678c4047351f470e2c
SylvainMJC <59646460+SylvainMJC@users.noreply.github.com>
2020-10-15 09:57:16 +0200

    bdd credentials

--- api/config/database.php
+++ api/config/database.php
@@ -2,12 +2,19 @@
 class Database{
   
     // specify your own database credentials
-    private $host = "localhost";
-    private $db_name = "api-db";
-    private $username = "root";
-    private $password = "";
+    // private $host = "localhost";
+    // private $db_name = "api-db";
+    // private $username = "root";
+    // private $password = "";
+    // public $conn;
+
+    // specify your own database credentials
+    private $host = "eu-cdbr-west-03.cleardb.net";
+    private $db_name = "heroku_19d2fdeaff209af";
+    private $username = "bc283985bfedac";
+    private $password = "b621df3b";
     public $conn;
-  
+  //mysql://bc283985bfedac:b621df3b@eu-cdbr-west-03.cleardb.net/heroku_19d2fdeaff209af?reconnect=true
     // get the database connection
     public function getConnection(){
   
